706a42a7a4f10084dcfb169f8885479acd98fa69
Sr-DP <88588188+Sr-DP@users.noreply.github.com>
2023-09-23 12:28:20 -0400

    Add files via upload
    Finalizacion de examen

--- Excersice.ts
+++ Excersice.ts
@@ -9,53 +9,127 @@ sin importar si es por título, autor o ISBN, complicando su implementación.
 
 Se ha identificado que la clase es muy difícil de mantener y modificar. Tu tarea es estudiar el código, identificar los problemas y
  considerar cómo podría refactorizarse para mejorar su diseño y estructura.*/
- class LibraryManagerEx {
-    books: any[] = [];
-    loans: any[] = [];
+class Book {
+    constructor(public title: string, public author: string, public ISBN: string) {}
+}
 
-    addBook(title: string, author: string, ISBN: string) {
-        this.books.push({ title, author, ISBN });
+class Loan {
+    constructor(public book: Book, public userID: string, public date: Date) {}
+}
+
+interface EmailServicioInterfaz{
+    sendEmail(userID: string, message: string)
+    
+}
+
+class EmailServicio implements EmailServicioInterfaz{
+    sendEmail(userID: string, message: string){
+        //enviar mensaje a usuario segun ID
+        console.log(`Enviando email a ${userID}: ${message}`);
+    }
+    
+}
+
+/*interface fechaDevolucion{
+    sendAlerta(date: Date)
+}*/
+
+
+
+
+
+
+
+
+class LibraryManagerEX {
+    private books: Book[] = [];
+    private loans: Loan[] = [];
+    public emailServicio: EmailServicio;
+
+     // notificaciones 
+
+    // mensaje email sevice
+
+    private constructor(emailServicio:EmailServicio){
+        this.emailServicio=emailServicio;
+
+    }
+
+     //notificacion
+     // Implementa la notificación a usuarios cuando se añade un libro
+     private notifyUsers(bookTitle: string) {
+        // Implementa la notificación a usuarios cuando se añade un libro
+    }
+
+
+    /*calcularMulta(libro: Libro): number {
+        if (!libro.fechaPrestamo || !libro.fechaDevolucion) {
+            throw new Error('El libro no ha sido prestado o devuelto correctamente.');
+        }
+
+        const diasRetraso = this.calcularDiasRetraso(libro.fechaPrestamo, libro.fechaDevolucion);
+        const multa = diasRetraso * 1; // $1 por día de retraso
+        return multa;
+    }
+
+    private calcularDiasRetraso(fechaPrestamo: Date, fechaDevolucion: Date): number {
+        const tiempoPrestamoEstandar = 7; // 7 días
+        const milisegundosPorDia = 24 * 60 * 60 * 1000; // 1 día en milisegundos
+
+        const diferenciaEnDias =
+            (fechaDevolucion.getTime() - fechaPrestamo.getTime()) / milisegundosPorDia;
+
+        return Math.max(0, diferenciaEnDias - tiempoPrestamoEstandar);
+    }*/
+    addBook(book: Book) {
+        this.books.push(book);
+        this.notifyUsers(book.title);
     }
 
     removeBook(ISBN: string) {
-        const index = this.books.findIndex(b => b.ISBN === ISBN);
+        const index = this.books.findIndex(book => book.ISBN === ISBN);
         if (index !== -1) {
             this.books.splice(index, 1);
         }
     }
 
-    search(query: string) {
-        // Usa el mismo método para buscar por título, autor o ISBN
-        return this.books.filter(book => 
-            book.title.includes(query) || 
-            book.author.includes(query) || 
-            book.ISBN === query
-        );
+    searchByTitle(title: string) {
+        return this.books.filter(book => book.title.includes(title));
+    }
+
+    searchByAuthor(author: string) {
+        return this.books.filter(book => book.author.includes(author));
+    }
+
+    searchByISBN(ISBN: string) {
+        return this.books.find(book => book.ISBN === ISBN);
     }
 
     loanBook(ISBN: string, userID: string) {
-        const book = this.books.find(b => b.ISBN === ISBN);
+        const book = this.searchByISBN(ISBN);
         if (book) {
-            this.loans.push({ ISBN, userID, date: new Date() });
-            this.sendEmail(userID, `Has solicitado el libro ${book.title}`);
+            this.loans.push(new Loan(book, userID, new Date()));
+            this.emailServicio.sendEmail(userID, `Has solicitado el libro ${book.title}`);
         }
     }
 
     returnBook(ISBN: string, userID: string) {
-        const index = this.loans.findIndex(loan => loan.ISBN === ISBN && loan.userID === userID);
+        const index = this.loans.findIndex(loan => loan.book.ISBN === ISBN && loan.userID === userID);
         if (index !== -1) {
-            this.loans.splice(index, 1);
-            this.sendEmail(userID, `Has devuelto el libro con ISBN ${ISBN}. ¡Gracias!`);
+            const returnedBook = this.loans.splice(index, 1)[0];
+            this.emailServicio.sendEmail(userID, `Has devuelto el libro con ISBN ${returnedBook.book.ISBN}. ¡Gracias!`);
         }
     }
 
-    sendEmail(userID: string, message: string) {
-        console.log(`Enviando email a ${userID}: ${message}`);
-        // Implementación ficticia del envío de correo
-    }
+   
 }
 
-const libraryex = new LibraryManagerEx();
+
+
+const libraryex = new LibraryManagerEX();
 libraryex.addBook("El Gran Gatsby", "F. Scott Fitzgerald", "123456789");
 libraryex.addBook("1984", "George Orwell", "987654321");
 libraryex.loanBook("123456789", "user01");
+
+
+
